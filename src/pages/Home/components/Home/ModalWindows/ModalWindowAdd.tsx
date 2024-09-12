37737ea0bb3b59b271df03ca40c932fe3077d8af
Retivov Pavlo <wertualyti@gmail.com>
2024-09-12 23:50:09 +0300

    add method add you photo

--- src/pages/Home/components/Home/ModalWindows/ModalWindowAdd.tsx
+++ src/pages/Home/components/Home/ModalWindows/ModalWindowAdd.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './ModalWindow.scss';
 import postData from '../../Backend/Post/post';
 import { checkTitle } from './FunctionForModalWindows/FunctionForModalWindows';
@@ -15,6 +15,10 @@ function ModalWindows({ fetchData, setModalWindowsAdd }: ModalProps): JSX.Elemen
   const [isError, setIsError] = useState(false);
   const [backgroundColor, setBackgroundColor] = useState('#ffffff');
   const [image] = useState([Cat, list, greatFon]);
+  const [imagePeople, setImagePeople] = useState('');
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [firstRender, setFirstRender] = useState(false);
 
   const setBoardName = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setTitle(event.target.value);
@@ -49,6 +53,55 @@ function ModalWindows({ fetchData, setModalWindowsAdd }: ModalProps): JSX.Elemen
     return url.startsWith('url');
   };
 
+  const readingFile = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (): void => {
+          const base64String = reader.result;
+          if (typeof base64String === 'string') {
+            setImagePeople(`url(${base64String})`);
+          }
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+  };
+
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>): void => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (): void => {
+        const base64String = reader.result;
+        if (typeof base64String === 'string') {
+          setImagePeople(`url(${base64String})`);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>): void => {
+    event.preventDefault(); // Запобігаємо забороненому ефекту "drop"
+  };
+
+  const triggerInputFile = (): void => {
+    fileInputRef?.current?.click();
+  };
+
+  useEffect(() => {
+    setBackgroundColor(imagePeople);
+    if (!firstRender) {
+      setFirstRender(true);
+    } else {
+      setIsEmpty(true);
+    }
+  }, [imagePeople, firstRender]);
+
   return (
     <div className="modal-windows" onClick={stopPropagation}>
       <label htmlFor="boardName">Назва дошки</label>
@@ -67,18 +120,48 @@ function ModalWindows({ fetchData, setModalWindowsAdd }: ModalProps): JSX.Elemen
       <div className="setFon">
         <div className="setFonColor">
           <input type="color" value={color} name="input2" id="colorName" onChange={setBoardColor} />
-          <figure
-            style={{
-              backgroundColor: !isImageUrl(backgroundColor) ? backgroundColor : 'none',
-              backgroundImage: isImageUrl(backgroundColor) ? backgroundColor : 'none',
-              backgroundSize: '100% 100%',
-            }}
-          />
+          <div>
+            <figure
+              style={{
+                backgroundColor: !isImageUrl(backgroundColor) ? backgroundColor : 'none',
+                backgroundImage: isImageUrl(backgroundColor) ? backgroundColor : 'none',
+                backgroundSize: '100% 100%',
+              }}
+            />
+            <h3>Тут зображено вибраний фон</h3>
+          </div>
         </div>
         <div className="setFonImage">
           <figure style={{ backgroundImage: image[0], backgroundSize: '100% 100%' }} onClick={setImage} />
           <figure style={{ backgroundImage: image[1], backgroundSize: '100% 100%' }} onClick={setImage} />
           <figure style={{ backgroundImage: image[2], backgroundSize: '100% 100%' }} onClick={setImage} />
+        </div>
+        <div className="setFinPeopleImage">
+          <div className="divImagePeople">
+            <input
+              className="inputImagePeople"
+              type="file"
+              onChange={readingFile}
+              name=""
+              id=""
+              ref={fileInputRef}
+              style={{ display: 'none' }}
+            />
+            <div onDrop={handleDrop} onDragOver={handleDragOver} onClick={triggerInputFile}>
+              <h2>Перетягніть зображення або виберіть його</h2>
+            </div>
+          </div>
+          <figure
+            style={{
+              backgroundColor: !isImageUrl(imagePeople) ? '#ffffff' : 'none',
+              backgroundImage: isImageUrl(imagePeople) ? imagePeople : 'none',
+              backgroundSize: '100% 100%',
+            }}
+            onClick={setImage}
+          >
+            {' '}
+            {!isEmpty ? <h2>Тут буде ваше зображення яке ви додали</h2> : null}{' '}
+          </figure>
         </div>
       </div>
       <button onClick={enter}>Створити дошку</button>
